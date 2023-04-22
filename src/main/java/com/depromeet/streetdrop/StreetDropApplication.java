4d0a5f3309db8f544316010662babcb0162a9fd9
siyeonSon <siyeon0209@naver.com>
2023-04-22 19:31:32 +0900

    :tada: initial commit

---
+++ src/main/java/com/depromeet/streetdrop/StreetDropApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.streetdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetDropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/streetdrop/StreetDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.streetdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
