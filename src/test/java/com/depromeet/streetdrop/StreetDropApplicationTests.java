f69fe7a736eaed9a177e04f9e6d52a8ee610c83f
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-05-02 19:42:34 +0900

    :bug: feat : remove SpringBootTest annotation for unit test

--- src/test/java/com/depromeet/streetdrop/StreetDropApplicationTests.java
+++ src/test/java/com/depromeet/streetdrop/StreetDropApplicationTests.java
@@ -1,13 +1,12 @@
 package com.depromeet.streetdrop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class StreetDropApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
