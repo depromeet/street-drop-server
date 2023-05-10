5c2cb22027dd213e06b5481adc38609b0187abab
YUN YOUNG <yunyoung1819@hanmail.net>
2023-05-10 23:23:35 +0900

    :sparkles: [feature] BaseTimeEntity, ResponseDto, PageResponseDto setting #16 (#17)
    * :sparkles: [feature] BaseTimeEntity, ResponseDto, PageResponseDto setting
    
    * :bug: [fix] healthController 애노테이션 수정

---
+++ src/main/java/com/depromeet/streetdrop/domain/common/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.depromeet.streetdrop.domain.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
+}
