a42744d0d87b41d204540b7623ab33fa048a4137
yunyoung1819 <yunyoung1819@hanmail.net>
2023-05-20 16:11:22 +0900

    [refactor] memberId 지역 변수로 처리

--- src/main/java/com/depromeet/streetdrop/apis/item/controller/ItemController.java
+++ src/main/java/com/depromeet/streetdrop/apis/item/controller/ItemController.java
@@ -6,6 +6,7 @@ import com.depromeet.streetdrop.domains.item.service.ItemService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +18,10 @@ public class ItemController {
 	private final ItemService itemService;
 
 	@Operation(summary = "드랍 아이템 등록")
-	@PostMapping("/{memberId}")
-	public ResponseEntity<Void> create(
-			@PathVariable Long memberId,
-			@RequestBody ItemRequestDto requestDto
-	) {
+	@PostMapping("/")
+	public ResponseEntity<Void> create(@RequestBody ItemRequestDto requestDto) {
+		// TODO: 로그인 적용 후 MEMBER ID 변경 필요
+		Long memberId = Long.valueOf(RandomStringUtils.random(15, false, true));
 		itemService.register(memberId, requestDto);
 		return ResponseDto.noContent();
 	}
