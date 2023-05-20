e1980acac10dea9608e66243b6f7b130d968dd08
yunyoung1819 <yunyoung1819@hanmail.net>
2023-05-20 23:31:43 +0900

    [refactor] ItemRequestDto 필드에 final 선언 추가

--- src/main/java/com/depromeet/streetdrop/domains/item/dto/request/ItemRequestDto.java
+++ src/main/java/com/depromeet/streetdrop/domains/item/dto/request/ItemRequestDto.java
@@ -11,42 +11,40 @@ import org.springframework.validation.annotation.Validated;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 @Validated
-//@RequiredArgsConstructor
 //@ParameterObject
 public class ItemRequestDto {
 
 	@Schema(description = "경도", example = "127.123456")
 	@IsLongitude
 	@NotNull(message = "Longitude is required")
-	private Double latitude;
+	private final Double latitude;
 
 	@Schema(description = "위도", example = "37.123456")
 	@IsLatitude
 	@NotNull(message= "Latitude is required")
-	private Double longitude;
+	private final Double longitude;
 
 	@Schema(description = "주소", example = "서울시 강남구 강남동")
-	private String address;
+	private final String address;
 
 	@Schema(description = "노래제목", example = "하입보이")
-	private String title;
+	private final String title;
 
 	@Schema(description = "아티스트명",  example = "New jeans")
-	private String artiest;
+	private final String artiest;
 
 	@Schema(description = "앨범명", example = "1st EP New jeans")
-	private String albumName;
+	private final String albumName;
 
 	@Schema(description = "앨범 커버 이미지")
-	private String albumImage;
+	private final String albumImage;
 
 	@Schema(description = "장르")
-	private List<String> genre;
+	private final List<String> genre;
 
 	@Schema(description = "콘텐츠", example = "블라블라")
-	private String content;
+	private final String content;
 
 	@Builder
 	public ItemRequestDto(Double latitude, Double longitude, String address, String title,
