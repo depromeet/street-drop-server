51f61ca199ecc7722ce035aa1ce27c8f8f478b12
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-05-25 22:58:33 +0900

    :bug: fix: add validation check

--- src/main/java/com/depromeet/streetdrop/domains/item/dto/request/ItemRequestDto.java
+++ src/main/java/com/depromeet/streetdrop/domains/item/dto/request/ItemRequestDto.java
@@ -3,6 +3,7 @@ package com.depromeet.streetdrop.domains.item.dto.request;
 import com.depromeet.streetdrop.domains.itemLocation.dto.request.LocationRequestDto;
 import com.depromeet.streetdrop.domains.music.dto.request.MusicRequestDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,9 +16,11 @@ import org.springframework.validation.annotation.Validated;
 public class ItemRequestDto {
 
 	@Schema(description = "위치 정보")
+	@Valid
 	private LocationRequestDto location;
 
 	@Schema(description = "음악 정보")
+	@Valid
 	private MusicRequestDto music;
 
 	@Schema(description = "콘텐츠", example = "블라블라")
