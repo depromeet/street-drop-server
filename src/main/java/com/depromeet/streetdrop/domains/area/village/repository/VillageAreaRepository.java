4dbdddcc7fb399e32172f81d5f8ba31edc60f847
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-05-25 20:13:37 +0900

    :bug: fix : can't find village bug fix (#51)
    * :bug: fix : can't find village bug fix
    
    * :bug: fix: add readonly transactional

--- src/main/java/com/depromeet/streetdrop/domains/area/village/repository/VillageAreaRepository.java
+++ src/main/java/com/depromeet/streetdrop/domains/area/village/repository/VillageAreaRepository.java
@@ -1,6 +1,7 @@
 package com.depromeet.streetdrop.domains.area.village.repository;
 
 import com.depromeet.streetdrop.domains.area.village.entity.VillageArea;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +9,8 @@ public interface VillageAreaRepository extends JpaRepository<VillageArea, Long> 
 
     @Query(value = "select count(i.id) from ItemLocation i where i.villageArea.villageName = :villageAreaName")
     int countItemsByVillageName(String villageAreaName);
+
+    @Query(value = "select v from VillageArea v where ST_Contains(v.villagePolygon, :point) = true")
+    VillageArea findVillageByLocationPoint(Point point);
 
 }
---
+++ src/main/java/com/depromeet/streetdrop/domains/area/village/service/VillageAreaService.java
@@ -0,0 +1,20 @@
+package com.depromeet.streetdrop.domains.area.village.service;
+
+import com.depromeet.streetdrop.domains.area.village.entity.VillageArea;
+import com.depromeet.streetdrop.domains.area.village.repository.VillageAreaRepository;
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class VillageAreaService {
+
+    private final VillageAreaRepository villageAreaRepository;
+
+    @Transactional(readOnly = true)
+    public VillageArea getVillageByLocationPoint(Point point) {
+        return villageAreaRepository.findVillageByLocationPoint(point);
+    }
+}
