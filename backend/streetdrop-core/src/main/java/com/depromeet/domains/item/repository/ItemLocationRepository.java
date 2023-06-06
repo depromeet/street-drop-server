240ae0d0f18b3e232dfa503a33e3b2f086b3de54
siyeonSon <siyeon0209@naver.com>
2023-06-06 23:56:07 +0900

    :bug: fix: add Param annotation in ItemRepository and ItemLocationRepository

--- backend/streetdrop-core/src/main/java/com/depromeet/domains/item/repository/ItemLocationRepository.java
+++ backend/streetdrop-core/src/main/java/com/depromeet/domains/item/repository/ItemLocationRepository.java
@@ -5,6 +5,7 @@ import com.depromeet.domains.item.entity.ItemLocation;
 import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +14,5 @@ public interface ItemLocationRepository extends JpaRepository<ItemLocation, Long
             "FROM ItemLocation il JOIN Item i on il.item.id = i.id " +
             "JOIN AlbumCover ac on il.item.albumCover.id = ac.id " +
             "WHERE ST_Distance_Sphere(il.point, :point) <= :distance")
-    List<ItemPointDao> findNearItemsPointsByDistance(Point point, Double distance);
+    List<ItemPointDao> findNearItemsPointsByDistance(@Param("point") Point point, @Param("distance") Double distance);
 }
--- backend/streetdrop-core/src/main/java/com/depromeet/domains/item/repository/ItemRepository.java
+++ backend/streetdrop-core/src/main/java/com/depromeet/domains/item/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package com.depromeet.domains.item.repository;
 
 import com.depromeet.domains.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("SELECT i FROM Item i " +
             "JOIN ItemLocation il on il.item.id = i.id " +
             "WHERE ST_Distance_Sphere(il.point, :point) <= :distance")
-    List<Item> findNearItemsByDistance(Point point, Double distance);
+    List<Item> findNearItemsByDistance(@Param("point") Point point, @Param("distance") Double distance);
 
 }
