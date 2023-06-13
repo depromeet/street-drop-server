38276ebce3a5d65e328936e2eaf11e827c8c218a
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-06-14 01:22:05 +0900

    :sparkles: Feat : add village info and drop count

--- backend/streetdrop-api/src/main/java/com/depromeet/apis/area/village/controller/VillageItemController.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/apis/area/village/controller/VillageItemController.java
@@ -1,16 +1,16 @@
 package com.depromeet.apis.area.village.controller;
 
 import com.depromeet.common.dto.ResponseDto;
+import com.depromeet.domains.area.village.dto.request.VillageItemsRequestDto;
 import com.depromeet.domains.area.village.dto.response.VillageItemsCountResponseDto;
 import com.depromeet.domains.area.village.service.VillageItemService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,11 +23,10 @@ public class VillageItemController {
 
     @Operation(summary = "동별 드랍 아이템 개수 조회")
     @GetMapping("/count")
-    public ResponseEntity<VillageItemsCountResponseDto> countItemsByVillage(
-            @Schema(description = "동 이름", example = "종로구 사직동")
-            @RequestParam(value = "name") String villageName
+    public ResponseEntity<VillageItemsCountResponseDto> countItemsInVillageByLocation(
+            @Valid VillageItemsRequestDto villageItemsRequestDto
     ) {
-        var response = villageItemService.countItemsByVillage(villageName);
+        var response = villageItemService.countItemsInVillageByLocation(villageItemsRequestDto);
         return ResponseDto.ok(response);
     }
 
