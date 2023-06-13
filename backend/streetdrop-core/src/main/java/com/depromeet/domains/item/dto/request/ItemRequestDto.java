ac07852607b7d6c1e82e262ac775db4b7f9d96c7
siyeonSon <87802191+siyeonSon@users.noreply.github.com>
2023-06-13 10:49:49 +0900

    :bug: fix: change to non-nullable content when posting item (#81)
    * :bug: fix: non-nullable content when posting item
    
    * :sparkles: chore: change to non-nullable content in Item

--- backend/streetdrop-core/src/main/java/com/depromeet/domains/item/dto/request/ItemRequestDto.java
+++ backend/streetdrop-core/src/main/java/com/depromeet/domains/item/dto/request/ItemRequestDto.java
@@ -3,6 +3,7 @@ package com.depromeet.domains.item.dto.request;
 import com.depromeet.domains.music.dto.request.MusicRequestDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,5 +24,6 @@ public class ItemRequestDto {
 	private MusicRequestDto music;
 
 	@Schema(description = "콘텐츠", example = "블라블라")
+	@NotNull(message = "Content is required")
 	private String content;
 }
