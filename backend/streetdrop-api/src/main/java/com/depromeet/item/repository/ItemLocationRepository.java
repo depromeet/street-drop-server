5f7c77e73f76e49cf6964c4ec593dde0769b17d8
siyeonSon <siyeon0209@naver.com>
2023-06-17 23:28:05 +0900

    :recycle: chore: fix ItemPointDao address

--- backend/streetdrop-api/src/main/java/com/depromeet/item/repository/ItemLocationRepository.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/item/repository/ItemLocationRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ItemLocationRepository extends JpaRepository<ItemLocation, Long> {
-    @Query("SELECT New com.depromeet.domains.item.dao.ItemPointDao(il.point, i.id, ac.albumThumbnail, (ST_Distance_Sphere(il.point, :point) <= :innerDistance)) " +
+    @Query("SELECT New com.depromeet.item.dao.ItemPointDao(il.point, i.id, ac.albumThumbnail, (ST_Distance_Sphere(il.point, :point) <= :innerDistance)) " +
             "FROM ItemLocation il JOIN Item i on il.item.id = i.id " +
             "JOIN AlbumCover ac on il.item.albumCover.id = ac.id " +
             "WHERE ST_Distance_Sphere(il.point, :point) <= :distance")
