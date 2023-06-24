f9a3eb66effca87828b70413a7f338574ffa107e
siyeonSon <siyeon0209@naver.com>
2023-06-24 16:20:39 +0900

    :sparkles: chore: add item report service

---
+++ backend/streetdrop-api/src/main/java/com/depromeet/item/service/ItemReportService.java
@@ -0,0 +1,40 @@
+package com.depromeet.item.service;
+
+import com.depromeet.common.error.dto.ErrorCode;
+import com.depromeet.common.error.exception.common.BusinessException;
+import com.depromeet.item.Item;
+import com.depromeet.item.ItemLike;
+import com.depromeet.item.ItemReport;
+import com.depromeet.item.dto.response.ItemLikeResponseDto;
+import com.depromeet.item.repository.ItemReportRepository;
+import com.depromeet.slack.SlackService;
+import com.depromeet.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ItemReportService {
+    private final ItemReportRepository itemReportRepository;
+    private final ItemService itemService;
+
+    @Transactional
+    public void reportItem(User user, Long itemId) {
+        var item = itemService.getItem(itemId);
+        checkUserAlreadyReport(user, item);
+
+        var itemReport = ItemReport.builder()
+                .item(item)
+                .user(user)
+                .build();
+        itemReportRepository.save(itemReport);
+    }
+
+    private void checkUserAlreadyReport(User user, Item item) {
+        boolean alreadyReported = itemReportRepository.existsByUserIdAndItemId(user.getId(), item.getId());
+        if (alreadyReported) {
+            throw new BusinessException(ErrorCode.ALREADY_ITEM_REPORTED_ERROR);
+        }
+    }
+}
