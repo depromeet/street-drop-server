6d3ca0faa80e8bea6cdaeccc0af5217ba6fe08ae
siyeonSon <siyeon0209@naver.com>
2023-06-24 16:21:32 +0900

    :sparkles: chore: add item report controller

---
+++ backend/streetdrop-api/src/main/java/com/depromeet/item/controller/ItemReportController.java
@@ -0,0 +1,32 @@
+package com.depromeet.item.controller;
+
+import com.depromeet.common.dto.ResponseDto;
+import com.depromeet.item.service.ItemReportService;
+import com.depromeet.security.annotation.ReqUser;
+import com.depromeet.user.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
+@Tag(name = "Items Report", description = "Item Report API")
+public class ItemReportController {
+    private final ItemReportService itemReportService;
+
+    @Operation(summary = "아이템 신고하기")
+    @PostMapping("/{itemId}/report")
+    public ResponseEntity<Void> reportItem(
+            @ReqUser User user,
+            @PathVariable Long itemId
+    ) {
+        itemReportService.reportItem(user, itemId);
+        return ResponseDto.noContent();
+    }
+}
