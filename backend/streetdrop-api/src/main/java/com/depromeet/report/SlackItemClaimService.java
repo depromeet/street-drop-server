954f92855dd1eda12a0fa805b7518a07ccdb112c
siyeonSon <siyeon0209@naver.com>
2023-06-24 20:43:28 +0900

    :sparkles: chore: add SlackItemClaimService

---
+++ backend/streetdrop-api/src/main/java/com/depromeet/report/SlackItemClaimService.java
@@ -0,0 +1,40 @@
+package com.depromeet.report;
+
+import com.depromeet.external.slack.SlackService;
+import com.depromeet.item.ItemClaim;
+import com.slack.api.model.block.LayoutBlock;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+
+@Component
+@RequiredArgsConstructor
+public class SlackItemClaimService {
+
+    private final SlackService slackService;
+    @Value("${slack.report-channel}")
+    private String slackReportingChannel;
+    private static final String TITLE = "Item Claim Report";
+
+    public void sendReport(ItemClaim itemClaim) {
+        var markdownTextObject = markdownText("*신고자:* " + itemClaim.getUser().getNickname()
+                + "\n*아이템 아이디:* " + itemClaim.getItem().getId()
+                + "\n*아이템 콘텐츠:* " + itemClaim.getItem().getContent()
+                + "\n*신고 시간:* " + itemClaim.getCreatedAt()
+        );
+
+        List<LayoutBlock> blocks = asBlocks(
+                header(h -> h.text(plainText(TITLE))),
+                section(s -> s.text(markdownTextObject))
+        );
+
+        slackService.sendMessage(blocks, slackReportingChannel);
+    }
+
+}
