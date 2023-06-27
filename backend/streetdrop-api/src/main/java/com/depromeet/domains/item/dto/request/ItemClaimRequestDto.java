78c6ac2aef6a2cb3d97edac210e65134c569c355
siyeonSon <siyeon0209@naver.com>
2023-06-27 21:53:04 +0900

    :recycle: chore: refactor from parameter to response body

--- backend/streetdrop-api/src/main/java/com/depromeet/domains/item/dto/request/ItemClaimRequestDto.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/item/dto/request/ItemClaimRequestDto.java
@@ -5,16 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Setter
-@Validated
-@ParameterObject
 public class ItemClaimRequestDto {
 
     @Schema(description = "아이템 아이디", example = "1")
@@ -22,7 +16,7 @@ public class ItemClaimRequestDto {
     public Long itemId;
 
     @Schema(description = "신고 사유", example = "욕설/비방")
-    @NotNull
+    @NotNull(message = "Claim reason is required")
     public String reason;
 
 }
