be17e763a378b0f9444a7cf48a093b99fde1006c
siyeonSon <siyeon0209@naver.com>
2023-07-05 14:49:40 +0900

    :sparkles: feat: remove musicApp from UserResponseDto

--- backend/streetdrop-api/src/main/java/com/depromeet/domains/user/dto/response/UserResponseDto.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/user/dto/response/UserResponseDto.java
@@ -9,16 +9,13 @@ public record UserResponseDto(
         @Schema(description = "사용자 닉네임", example = "사용자 1")
         String nickname,
         @Schema(description = "사용자 프로필 이미지 URL")
-        String profileImage,
-        @Schema(description = "사용자 음악앱", example = "youtubemusic")
-        String musicApp
+        String profileImage
 ) {
     public UserResponseDto(User user) {
         this(
                 user.getId(),
                 user.getNickname(),
-                "https://s3.orbi.kr/data/file/united/35546557a06831597f6e7851cb6c86e9.jpg",
-                "youtubemusic"
+                "https://s3.orbi.kr/data/file/united/35546557a06831597f6e7851cb6c86e9.jpg"
         );
     }
 }
