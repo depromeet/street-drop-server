1050ab02fdf6dfefc19f29f7f2a57937aba43288
siyeonSon <87802191+siyeonSon@users.noreply.github.com>
2023-07-15 21:51:28 +0900

    ✨ feat(notification): change memory storage -> mongoDB storage (#210)
    * :sparkles: feat: create UserDeviceRepository
    
    * :sparkles: chore: find device token by userId
    
    * :sparkles: chore: find all device tokens
    
    * :sparkles: feat: save token
    
    * :sparkles: feat: update token
    
    * :sparkles: feat: delete token
    
    * :sparkles: chore: combine saveToken and insertToken
    
    * :sparkles: chore: add Transactional annotation
    
    * :sparkles: chore: change TokenRepository -> UserDeviceRepository in PushService
    
    * :sparkles: chore: change TokenRepository -> UserDeviceRepository in TopicService
    
    * :sparkles: chore: delete TokenRepository and MemoryTokenRepository
    
    * :sparkles: chore: rename createToken -> saveToken
    
    * :sparkles: feat: save notification
    
    * :bug: fix: fix Query annotation of findAllDeviceTokens
    
    * :recycle: refactor: delete UserDevice
    
    * :recycle: refactor: delete deleteToken service logic
    
    * :recycle: refactor: change findDeviceTokenByUserId -> findByUserId
    
    * :recycle: refactor: find tokens using findDeviceTokenByUserId -> findByUserId
    
    * :recycle: refactor: add transactional annotation
    
    * :recycle: refactor: extract getting tokens logic into private method
    
    * :recycle: refactor: give space
    
    * :recycle: refactor: change token not found error message
    
    * :fire: chore: remove Param annotation
    
    * :recycle: refactor: find tokens using findByUserId -> findDeviceTokenByUserId
    
    * :recycle: refactor: change token not found error message
    
    * :recycle: refactor: remove unused import statement
    
    * :recycle: refactor: notificationList save -> saveAll

--- backend/streetdrop-notification/src/main/java/com/depromeet/service/TokenService.java
+++ backend/streetdrop-notification/src/main/java/com/depromeet/service/TokenService.java
@@ -1,25 +1,39 @@
 package com.depromeet.service;
 
+import com.depromeet.domain.UserDevice;
 import com.depromeet.dto.request.TokenRequestDto;
-import com.depromeet.repository.TokenRepository;
+import com.depromeet.repository.UserDeviceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
 
-    private final TokenRepository tokenRepository;
+    private final UserDeviceRepository userDeviceRepository;
 
-    public void createToken(TokenRequestDto tokenRequestDto) {
-        tokenRepository.save(tokenRequestDto.getUserId(), tokenRequestDto.getToken());
+    @Transactional
+    public void saveToken(TokenRequestDto tokenRequestDto) {
+        Optional<UserDevice> userDevice = userDeviceRepository.findByUserId(tokenRequestDto.getUserId());
+        if (userDevice.isPresent()) {
+            UserDevice updatedUserDevice = userDevice.get().updateDeviceToken(tokenRequestDto.getToken());
+            userDeviceRepository.save(updatedUserDevice);
+        } else {
+            UserDevice createdUserDevice = UserDevice.builder()
+                    .userId(tokenRequestDto.getUserId())
+                    .deviceToken(tokenRequestDto.getToken())
+                    .build();
+            userDeviceRepository.save(createdUserDevice);
+        }
     }
 
-    public void updateToken(TokenRequestDto tokenRequestDto) {
-        tokenRepository.update(tokenRequestDto.getUserId(), tokenRequestDto.getToken());
-    }
-
+    @Transactional
     public void deleteToken(Long userId) {
-        tokenRepository.delete(userId);
+        var userDevice = userDeviceRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("Token not found for userId: " + userId));
+        userDeviceRepository.delete(userDevice);
     }
 }
--- backend/streetdrop-notification/src/main/java/com/depromeet/service/TopicService.java
+++ backend/streetdrop-notification/src/main/java/com/depromeet/service/TopicService.java
@@ -2,7 +2,7 @@ package com.depromeet.service;
 
 import com.depromeet.dto.request.TopicSubscribeRequestDto;
 import com.depromeet.external.fcm.FcmService;
-import com.depromeet.repository.TokenRepository;
+import com.depromeet.repository.UserDeviceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +12,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TopicService {
 
-    private final TokenRepository tokenRepository;
+    private final UserDeviceRepository userDeviceRepository;
     private final FcmService fcmService;
 
     public void subscribeTopic(TopicSubscribeRequestDto topicSubscribeRequestDto) {
-        List<String> tokens = tokenRepository.findByUserIds(topicSubscribeRequestDto.getUserIds());
+        List<String> tokens = getTokens(topicSubscribeRequestDto.getUserIds());
         try {
             fcmService.subscribeTopicSync(topicSubscribeRequestDto.getTopic(), tokens);
         } catch (Exception e) {
@@ -24,11 +24,18 @@ public class TopicService {
     }
 
     public void unsubscribeTopic(TopicSubscribeRequestDto topicSubscribeRequestDto) {
-        List<String> tokens = tokenRepository.findByUserIds(topicSubscribeRequestDto.getUserIds());
+        List<String> tokens = getTokens(topicSubscribeRequestDto.getUserIds());
         try {
             fcmService.subscribeTopicSync(topicSubscribeRequestDto.getTopic(), tokens);
         } catch (Exception e) {
         }
+    }
+
+    private List<String> getTokens(List<Long> userIds) {
+        return userIds.stream()
+                .map(userId -> userDeviceRepository.findDeviceTokenByUserId(userId)
+                        .orElseThrow(() -> new RuntimeException("Token not found for userId: " + userId)))
+                .toList();
     }
 
 }
