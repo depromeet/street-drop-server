012f7104c1907a88b47920fd62400a3f503ce768
yunyoung1819 <yunyoung1819@hanmail.net>
2023-07-15 21:51:14 +0900

    :sparkles: feat(api): Add AWS S3 Config, S3 Service

---
+++ backend/streetdrop-api/src/main/java/com/depromeet/external/aws/s3/AwsS3Service.java
@@ -0,0 +1,29 @@
+package com.depromeet.external.aws.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+	private final AmazonS3 amazonS3;
+
+	public static final String DIR_NAME = "USER_PROFILE";
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	public String getS3(String bucket, String fileName) {
+		return amazonS3.getUrl(bucket, fileName).toString();
+	}
+
+	public String getS3FilePaths() {
+		return bucket;
+	}
+
+	public String getFileName() {
+		return DIR_NAME + "/" + "Level1.png";
+	}
+}
