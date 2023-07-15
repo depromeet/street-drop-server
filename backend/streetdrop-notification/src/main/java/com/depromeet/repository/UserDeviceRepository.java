1eb2bcf41ebb87b2fb7c7cbd01a14cb421f27375
siyeonSon <siyeon0209@naver.com>
2023-07-15 19:17:18 +0900

    :fire: chore: remove Param annotation

--- backend/streetdrop-notification/src/main/java/com/depromeet/repository/UserDeviceRepository.java
+++ backend/streetdrop-notification/src/main/java/com/depromeet/repository/UserDeviceRepository.java
@@ -3,15 +3,17 @@ package com.depromeet.repository;
 import com.depromeet.domain.UserDevice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserDeviceRepository extends MongoRepository<UserDevice, String> {
 
-    @Query(value = "{ 'userId' : userId }")
-    Optional<UserDevice> findByUserId(@Param("userId") Long userId);
+    @Query(value = "{ 'userId' : ?0 }")
+    Optional<UserDevice> findByUserId(Long userId);
+
+    @Query(value = "{ 'userId' : ?0 }", fields = "{ 'deviceToken' : 1 }")
+    Optional<String> findDeviceTokenByUserId(Long userId);
 
     @Query(value = "{}", fields = "{ 'deviceToken' : 1 }")
     List<String> findAllDeviceTokens();
