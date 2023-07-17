c1d937c39a708dbf5625963dd90594d839f82529
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-07-17 16:01:20 +0900

    :bug: hotfix(api): api import hotfix (#235)

--- backend/streetdrop-api/src/main/java/com/depromeet/domains/user/repository/BlockUserRepository.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/user/repository/BlockUserRepository.java
@@ -3,6 +3,7 @@ package com.depromeet.domains.user.repository;
 import com.depromeet.user.BlockUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
