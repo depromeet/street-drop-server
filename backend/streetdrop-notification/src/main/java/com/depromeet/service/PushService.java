9a19cb5bbf15ffd6ea2cb6dc822945086f2938e7
siyeonSon <87802191+siyeonSon@users.noreply.github.com>
2023-07-18 20:21:56 +0900

    🚑 hotfix(notification): find tokens using findDeviceTokenByUserId -> findByUserId (#237)
    * :ambulance: hotfix: find tokens using findDeviceTokenByUserId -> findByUserId
    
    * :fire: fix: remove findDeviceTokenByUserId

--- backend/streetdrop-notification/src/main/java/com/depromeet/service/PushService.java
+++ backend/streetdrop-notification/src/main/java/com/depromeet/service/PushService.java
@@ -1,5 +1,6 @@
 package com.depromeet.service;
 
+import com.depromeet.domain.UserDevice;
 import com.depromeet.dto.request.AllPushRequestDto;
 import com.depromeet.dto.request.PushRequestDto;
 import com.depromeet.dto.request.TopicPushRequestDto;
@@ -23,8 +24,9 @@ public class PushService {
     @Transactional
     public void sendPush(PushRequestDto pushRequestDto) {
         List<String> tokens = pushRequestDto.getUserIds().stream()
-                .map(userId -> userDeviceRepository.findDeviceTokenByUserId(userId)
+                .map(userId -> userDeviceRepository.findByUserId(userId)
                         .orElseThrow(() -> new RuntimeException("Token not found for userId: " + userId)))
+                .map(UserDevice::getDeviceToken)
                 .toList();
 
         try {
