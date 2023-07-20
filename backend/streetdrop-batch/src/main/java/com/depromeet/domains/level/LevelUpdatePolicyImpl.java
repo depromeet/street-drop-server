c9627067163caf1a8072ec4f0150e3d16242ba0d
yunyoung1819 <yunyoung1819@hanmail.net>
2023-07-20 19:40:15 +0900

    :recycle: move to comment and add @AllArgsConstructor annotation

--- backend/streetdrop-batch/src/main/java/com/depromeet/domains/level/LevelUpdatePolicyImpl.java
+++ backend/streetdrop-batch/src/main/java/com/depromeet/domains/level/LevelUpdatePolicyImpl.java
@@ -1,15 +1,19 @@
 package com.depromeet.domains.level;
 
 import com.depromeet.user.UserLevel;
+import lombok.AllArgsConstructor;
 
+/**
+ * 기능 요구 사항
+ * 등급    레벨명        조건
+ *  1   스트릿드랍퍼   드랍횟수 0~4
+ *  2   열정드랍퍼    드랍횟수 5~24
+ *  3   스페셜 DJ    드랍횟수 25~54
+ */
+@AllArgsConstructor
 public class LevelUpdatePolicyImpl implements LevelUpdatePolicy {
 	private final int levelCondition;
 	private final UserLevel newLevel;
-
-	public LevelUpdatePolicyImpl(int levelCondition, UserLevel userLevel) {
-		this.levelCondition = levelCondition;
-		this.newLevel = userLevel;
-	}
 
 	@Override
 	public boolean isAcceptable(int itemCount) {
