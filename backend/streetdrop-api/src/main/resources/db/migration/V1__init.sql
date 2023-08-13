1994a65c347ef05677dd958d6608334ea9f51cde
siyeonSon <siyeon0209@naver.com>
2023-08-13 19:51:27 +0900

    :sparkles: chore(api): add V1__init.sql

--- backend/streetdrop-api/src/main/resources/db/migration/V1__init.sql
+++ backend/streetdrop-api/src/main/resources/db/migration/V1__init.sql
@@ -1,163 +1,206 @@
-create table album_cover
+create table if not exists album_cover
 (
     album_cover_id  bigint auto_increment
         primary key,
-    album_image     varchar(255) not null,
-    album_thumbnail varchar(255) not null
-);
+    created_at      datetime(6)  null,
+    modified_at     datetime(6)  null,
+    album_image     varchar(255) null,
+    album_thumbnail varchar(255) null
+)
+    charset = utf8mb3;
 
-create table artist
+create table if not exists artist
 (
-    artist_id bigint auto_increment
+    artist_id   bigint auto_increment
         primary key,
-    name      varchar(255) not null
-);
+    created_at  datetime(6)  null,
+    modified_at datetime(6)  null,
+    name        varchar(255) null
+)
+    charset = utf8mb3;
 
-create table album
+create table if not exists album
 (
     album_id       bigint auto_increment
         primary key,
-    name           varchar(255) not null,
-    album_cover_id bigint       not null,
-    artist_id      bigint       not null,
-    constraint fk_album_album_cover_id
-        foreign key (album_cover_id) references album_cover (album_cover_id),
-    constraint fk_album_artist_id
-        foreign key (artist_id) references artist (artist_id)
-);
+    created_at     datetime(6)  null,
+    modified_at    datetime(6)  null,
+    name           varchar(255) null,
+    album_cover_id bigint       null,
+    artist_id      bigint       null,
+    constraint FK7otyksnaj781l0b2r8570vefc
+        foreign key (album_cover_id) references streetdrop.album_cover (album_cover_id),
+    constraint FKmwc4fyyxb6tfi0qba26gcf8s1
+        foreign key (artist_id) references streetdrop.artist (artist_id)
+)
+    charset = utf8mb3;
 
-create table block_user
+create table if not exists block_user
 (
     block_id    bigint auto_increment
         primary key,
+    blocked_id  bigint      null,
+    blocker_id  bigint      null,
     created_at  datetime(6) not null,
-    modified_at datetime(6) not null,
-    blocked_id  bigint      not null,
-    blocker_id  bigint      not null
+    modified_at datetime(6) not null
 );
 
-create table default_nick_name
+create table if not exists default_nick_name
 (
     id             bigint auto_increment
         primary key,
-    post_nick_name varchar(5) not null,
-    pre_nick_name  varchar(5) not null
+    post_nick_name varchar(5) null,
+    pre_nick_name  varchar(5) null
 );
 
-create table genre
+create table if not exists genre
 (
-    genre_id bigint auto_increment
+    genre_id    bigint auto_increment
         primary key,
-    name     varchar(255) not null
-);
+    created_at  datetime(6)  null,
+    modified_at datetime(6)  null,
+    name        varchar(255) null
+)
+    charset = utf8mb3;
 
-create table item_claim
+create table if not exists item_claim
 (
     item_claim_id bigint auto_increment
         primary key,
-    created_at    datetime(6)  not null,
-    modified_at   datetime(6)  not null,
-    item_id       bigint       not null,
+    created_at    datetime(6)  null,
+    modified_at   datetime(6)  null,
+    item_id       bigint       null,
+    user_id       bigint       null,
     reason        varchar(255) not null,
-    status        varchar(255) not null,
-    user_id       bigint       not null
+    status        varchar(255) null
 );
 
-create table item_like
+create table if not exists member
 (
-    item_like_id bigint auto_increment
+    id       bigint auto_increment
         primary key,
-    created_at   datetime(6) not null,
-    modified_at  datetime(6) not null,
-    item_id      bigint      null,
-    user_id      bigint      null
-);
-
-create table song
-(
-    song_id  bigint auto_increment
-        primary key,
+    email    varchar(255) not null,
     name     varchar(255) not null,
-    album_id bigint       not null,
-    constraint fk_song_album_id
-        foreign key (album_id) references album (album_id)
+    part     varchar(255) not null,
+    password varchar(255) not null,
+    username varchar(255) not null
 );
 
-create table song_genre
+create table if not exists song
+(
+    song_id     bigint auto_increment
+        primary key,
+    created_at  datetime(6)  null,
+    modified_at datetime(6)  null,
+    name        varchar(255) null,
+    album_id    bigint       null,
+    constraint FKrcjmk41yqj3pl3iyii40niab0
+        foreign key (album_id) references streetdrop.album (album_id)
+)
+    charset = utf8mb3;
+
+create table if not exists song_genre
 (
     song_genre_id bigint auto_increment
         primary key,
-    genre_id      bigint not null,
-    song_id       bigint not null,
-    constraint fk_song_genre_song_id
-        foreign key (song_id) references song (song_id),
-    constraint fk_song_genre_genre_id
-        foreign key (genre_id) references genre (genre_id)
-);
+    created_at    datetime(6) null,
+    modified_at   datetime(6) null,
+    genre_id      bigint      null,
+    song_id       bigint      null,
+    constraint FK1ssu87dg5vsdxpmyjqqc42if3
+        foreign key (song_id) references streetdrop.song (song_id),
+    constraint FKmpuht870e976moxtxywrfngcr
+        foreign key (genre_id) references streetdrop.genre (genre_id)
+)
+    charset = utf8mb3;
 
-create table state_area
+create table if not exists state_area
 (
     state_id   bigint auto_increment
         primary key,
     state_code int          not null,
     state_name varchar(255) not null,
     version    int          not null
-);
+)
+    charset = utf8mb3;
 
-create table city_area
+create table if not exists city_area
 (
     city_id   bigint auto_increment
         primary key,
     city_code int          not null,
     city_name varchar(255) not null,
     version   int          not null,
-    state_id  bigint       not null,
-    constraint fk_city_area_state_id
-        foreign key (state_id) references state_area (state_id)
-);
+    state_id  bigint       null,
+    constraint FKrf563rtlt9al5bw1lekn18du1
+        foreign key (state_id) references streetdrop.state_area (state_id)
+)
+    charset = utf8mb3;
 
-create table users_level
-(
-    user_level_id bigint auto_increment
-        primary key,
-    description   varchar(255) not null,
-    image         varchar(255) not null,
-    name          varchar(255) not null
-);
-
-create table users
+create table if not exists users
 (
     user_id       bigint auto_increment
         primary key,
-    created_at    datetime(6)  not null,
-    modified_at   datetime(6)  not null,
-    idfv          varchar(100) not null,
-    music_app     varchar(255) not null,
-    nickname      varchar(20)  not null,
-    user_level_id bigint       not null,
-    constraint fk_users_user_level_id
-        foreign key (user_level_id) references users_level (user_level_id)
-);
+    created_at    datetime(6)  null,
+    modified_at   datetime(6)  null,
+    idfv          varchar(100) null,
+    nickname      varchar(20)  null,
+    music_app     varchar(255) null,
+    user_level_id bigint       null
+)
+    charset = utf8mb3;
 
-create table item
+create table if not exists item
 (
     item_id        bigint auto_increment
         primary key,
-    created_at     datetime(6)  not null,
-    modified_at    datetime(6)  not null,
-    content        varchar(500) not null,
-    album_cover_id bigint       not null,
-    song_id        bigint       not null,
-    user_id        bigint       not null,
-    constraint fk_item_user_id
-        foreign key (user_id) references users (user_id),
-    constraint fk_item_song_id
-        foreign key (song_id) references song (song_id),
-    constraint fk_item_album_cover_id
-        foreign key (album_cover_id) references album_cover (album_cover_id)
+    created_at     datetime(6)                             null,
+    modified_at    datetime(6)                             null,
+    content        varchar(500) collate utf8mb4_general_ci null,
+    album_cover_id bigint                                  null,
+    song_id        bigint                                  null,
+    user_id        bigint                                  null,
+    constraint FKc4s174l330le17rblwgyjawev
+        foreign key (user_id) references streetdrop.users (user_id),
+    constraint FKf9j8ocdanjs7a48djb3aqd0en
+        foreign key (song_id) references streetdrop.song (song_id),
+    constraint FKip7vjlqnw0rt125paig31ni3
+        foreign key (album_cover_id) references streetdrop.album_cover (album_cover_id)
+)
+    charset = utf8mb3;
+
+create table if not exists item_like
+(
+    item_like_id bigint auto_increment
+        primary key,
+    created_at   datetime(6) null,
+    modified_at  datetime(6) null,
+    item_id      bigint      null,
+    user_id      bigint      null,
+    constraint FK943xouml6m8ikrxn862iq9qhj
+        foreign key (item_id) references streetdrop.item (item_id),
+    constraint FKIc81jr021caudusp3hcfjr
+        foreign key (user_id) references streetdrop.users (user_id)
+)
+    charset = utf8mb3;
+
+create index FK943xouml6m8ikrxn862iq9qhj_idx
+    on streetdrop.item_like (item_id);
+
+create index FKIc81jr021caudusp3hcfjr_idx
+    on streetdrop.item_like (user_id);
+
+create table streetdrop.users_level
+(
+    level_id      bigint auto_increment
+        primary key,
+    description   varchar(255) not null,
+    image         varchar(255) not null,
+    name          varchar(255) not null,
+    user_level_id bigint       not null
 );
 
-create table village_area
+create table if not exists village_area
 (
     village_id           bigint auto_increment
         primary key,
@@ -166,23 +209,27 @@ create table village_area
     village_code         int          not null,
     village_name         varchar(255) not null,
     village_polygon      multipolygon not null,
-    city_id              bigint       not null,
-    constraint fk_village_area_city_id
-        foreign key (city_id) references city_area (city_id)
+    city_id              bigint       null,
+    constraint FKadtch2u4brlxykg1e03xdkyiw
+        foreign key (city_id) references streetdrop.city_area (city_id)
 );
 
-create table item_location
+create table if not exists item_location
 (
     item_location_id bigint auto_increment
         primary key,
-    created_at       datetime(6)  not null,
-    modified_at      datetime(6)  not null,
+    created_at       datetime(6)  null,
+    modified_at      datetime(6)  null,
     name             varchar(255) null,
     point            point        null,
-    item_id          bigint       not null,
-    village_id       bigint       not null,
-    constraint fk_item_location_item_id
-        foreign key (item_id) references item (item_id),
-    constraint fk_item_location_village_id
-        foreign key (village_id) references village_area (village_id)
-);
+    item_id          bigint       null,
+    village_id       bigint       null,
+    constraint FK50oxdirrbr9uk55y7xlrq6jmv
+        foreign key (item_id) references streetdrop.item (item_id),
+    constraint FKlnnflryk5f1y35ismb0qpnepy
+        foreign key (village_id) references streetdrop.village_area (village_id)
+)
+    charset = utf8mb3;
+
+create spatial index idx_coordinates
+    on streetdrop.village_area (village_polygon);
