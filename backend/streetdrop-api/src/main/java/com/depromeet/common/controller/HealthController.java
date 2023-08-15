78fae3de7cf20be49d1200b5dac747b626b4511b
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-08-15 19:59:59 +0900

    :bug: fix(api) : add / to health controller

--- backend/streetdrop-api/src/main/java/com/depromeet/common/controller/HealthController.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/common/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping({"/", "/health"})
     public String health() {
         return "Application Health Good!";
     }
