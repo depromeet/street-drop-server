3837d3a209e684ea39075f176f13b043c20f61fd
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-08-16 12:30:48 +0900

    :recycle: refactor(admin): change apis dir to common, domains

---
+++ backend/streetdrop-admin/streetdrop-admin-server/src/main/java/com/depromeet/domains/user/controller/UserStaticalController.java
@@ -0,0 +1,36 @@
+package com.depromeet.domains.user.controller;
+
+import com.depromeet.domains.user.dto.UserAllStaticCountDto;
+import com.depromeet.domains.user.dto.UserSignUpCountRequestDto;
+import com.depromeet.domains.user.dto.UserSignUpCountResponseDto;
+import com.depromeet.domains.user.service.UserStaticService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/statical")
+@RequiredArgsConstructor
+public class UserStaticalController {
+
+    private final UserStaticService userService;
+
+    @GetMapping("/signup/count")
+    public ResponseEntity<List<UserSignUpCountResponseDto>> getUserSignUpCount(
+            UserSignUpCountRequestDto userSignUpCountRequestDto
+    ) {
+        var response = userService.getUserSignUpCount(userSignUpCountRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/all/count")
+    public ResponseEntity<UserAllStaticCountDto> getAllUserCount() {
+        var response = userService.getAllUserCount();
+        return ResponseEntity.ok(response);
+    }
+
+}
