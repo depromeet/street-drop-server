8e4ea18314e36b4cf52daf6a6aced25690da7f2b
yunyoung1819 <yunyoung1819@hanmail.net>
2023-10-29 20:01:13 +0900

    :sparkles: feat(api): Add discord webhook - user blocked

--- backend/streetdrop-api/src/test/java/unit/external/discord/DiscordServiceTest.java
+++ backend/streetdrop-api/src/test/java/unit/external/discord/DiscordServiceTest.java
@@ -10,13 +10,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class DiscordServiceTest {
-	public static final String WEB_HOOK_URL = "https://discord.com/api/webhooks/1165606257181327360/04XWl4J0Xq2e-2Cg7tj_rXHxnqd8DuB0pwwKc2Jqi7jmct7k6EE4Tw6IJWcoBxcrCFul";
+	public static final String WEB_HOOK_URL = "TEST";
 	public static final String CHANNEL = "claim";
 
 	@DisplayName("디스코드 메시지 테스트")
 	@Test
 	public void messageTest() throws Exception {
 		DiscordService discordService = new DiscordService();
-		discordService.sendMessages("테스트 입니다", CHANNEL);
+		discordService.sendMessages(WEB_HOOK_URL,"테스트 입니다", CHANNEL);
 	}
 }
