a0c4d0c7642fbdfcb1b4351e1f4d82e202c98fae
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2024-01-27 16:13:07 +0900

    :sparkles: feat: change reflection to ClassPathScanningCandidateComponentProvider

--- backend/streetdrop-api/src/main/java/com/depromeet/common/error/dto/StreetDropErrorCodeList.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/common/error/dto/StreetDropErrorCodeList.java
@@ -1,14 +1,20 @@
 package com.depromeet.common.error.dto;
 
+import com.depromeet.common.error.dto.interfaces.ErrorCode;
 import com.depromeet.common.error.dto.interfaces.ErrorCodeInterface;
 import lombok.Getter;
-import org.reflections.Reflections;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AssignableTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 @Getter
+@Slf4j
 public class StreetDropErrorCodeList {
 
     private static volatile StreetDropErrorCodeList instance;
@@ -31,19 +37,31 @@ public class StreetDropErrorCodeList {
 
     private synchronized List<StreetDropErrorCode> createStreetDropErrorCodeList() {
         List<StreetDropErrorCode> streetDropErrorCodeList = new ArrayList<>();
-        Set<Class<? extends ErrorCodeInterface>> list = new Reflections().getSubTypesOf(ErrorCodeInterface.class);
-        for (Class<? extends ErrorCodeInterface> errorCodes : list) {
-            if (errorCodes.isEnum()){
-                for (var errorCode : errorCodes.getEnumConstants()) {
-                    if (errorCode != null){
-                        var errorResponseCode = errorCode.getErrorResponseCode();
-                        var error = errorCode.toErrorCode();
-                        var streetDropError = new StreetDropErrorCode(errorResponseCode, error);
-                        streetDropErrorCodeList.add(streetDropError);
+
+        try {
+            ClassPathScanningCandidateComponentProvider s = new ClassPathScanningCandidateComponentProvider(false);
+
+            TypeFilter tf = new AssignableTypeFilter(ErrorCodeInterface.class);
+            s.addIncludeFilter(tf);
+
+            Set<BeanDefinition> components = s.findCandidateComponents("com.depromeet");
+
+            for (BeanDefinition component : components) {
+                Class<?> className = Class.forName(component.getBeanClassName());
+
+                if (className.isEnum()) {
+                    for (var errorCode : className.getEnumConstants()) {
+                        if (errorCode != null) {
+                            String errorResponseCode = (String) errorCode.getClass().getMethod("getErrorResponseCode").invoke(errorCode);
+                            ErrorCode error = (ErrorCode) errorCode.getClass().getMethod("toErrorCode").invoke(errorCode);
+                            var streetDropError = new StreetDropErrorCode(errorResponseCode, error);
+                            streetDropErrorCodeList.add(streetDropError);
+                        }
                     }
                 }
             }
 
+        } catch (Exception ignored) {
         }
 
         return streetDropErrorCodeList;
