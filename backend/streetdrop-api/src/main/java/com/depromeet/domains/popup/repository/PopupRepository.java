605e8752982bdaad6dd564c932f7dbad9d43c073
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2024-03-12 21:56:38 +0900

    :bug: fix: query findByUserId fix typo error

--- backend/streetdrop-api/src/main/java/com/depromeet/domains/popup/repository/PopupRepository.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/popup/repository/PopupRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PopupRepository extends JpaRepository<Popup, Long> {
-    @Query("SELECT p FROM Popup p WHERE p.isRead = false AND P.userId = :id")
+    @Query("SELECT p FROM Popup p WHERE p.isRead = false AND p.userId = :id")
     List<Popup> findAllByUserId(Long id);
 }
