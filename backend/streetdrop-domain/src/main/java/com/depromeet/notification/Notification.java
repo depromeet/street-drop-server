65beb30946f4c6d60904fb2f01b285febd981698
siyeonSon <siyeon0209@naver.com>
2024-04-04 16:02:19 +0900

    :recycle: refactor(domain): change mongodb -> mysql

---
+++ backend/streetdrop-domain/src/main/java/com/depromeet/notification/Notification.java
@@ -0,0 +1,58 @@
+package com.depromeet.notification;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.depromeet.common.entity.BaseTimeEntity;
+import com.depromeet.notification.vo.NotificationType;
+import com.depromeet.user.UserDevice;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    @Enumerated(STRING)
+    @Column(nullable = false)
+    private NotificationType notificationType;
+
+    @Column(nullable = false)
+    private boolean isVeiwed;
+
+    @ManyToOne(fetch = LAZY, optional = false)
+    @JoinColumn(name = "user_device_id", insertable = false, updatable = false, nullable = false)
+    private UserDevice userDevice;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "notification_action_id")
+    private NotificationAction notificationAction;
+
+    @Builder
+    public Notification(String title, String content, NotificationType notificationType,
+                        UserDevice userDevice, NotificationAction notificationAction) {
+        this.title = title;
+        this.content = content;
+        this.notificationType = notificationType;
+        this.isVeiwed = false;
+        this.userDevice = userDevice;
+        this.notificationAction = notificationAction;
+    }
+
+}
