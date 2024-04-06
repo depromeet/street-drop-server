5aa30a2adb578d55545c4f699675b4560d451495
siyeonSon <siyeon0209@naver.com>
2024-04-06 21:50:40 +0900

    :recycle: refactor(notification): add custom ErrorCode

--- backend/streetdrop-batch/src/main/java/com/depromeet/common/error/code/GlobalErrorCode.java
+++ backend/streetdrop-batch/src/main/java/com/depromeet/common/error/code/GlobalErrorCode.java
@@ -15,7 +15,8 @@ public enum GlobalErrorCode implements ErrorCodeInterface {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", "Internal Server Error"),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid Input Value", "Invalid Input Value"),
     EXTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "External Sever Error","Invalid Input Value"),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "Authentication is required and has failed or has not been provided.");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "Authentication is required and has failed or has not been provided."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User is not found", "User is not found");
 
     private final HttpStatus status;
     private final String code = name().toUpperCase();
