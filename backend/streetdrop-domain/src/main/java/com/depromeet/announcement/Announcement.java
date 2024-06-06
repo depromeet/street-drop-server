cd22ed2413a64c88abac2b5f18abe771c6049d4e
siyeonSon <siyeon0209@naver.com>
2024-06-06 14:06:38 +0900

    :sparkles: feat(domain): add announcement entity

---
+++ backend/streetdrop-domain/src/main/java/com/depromeet/announcement/Announcement.java
@@ -0,0 +1,30 @@
+package com.depromeet.announcement;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import com.depromeet.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Announcement extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "announcement_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String content;
+
+}
