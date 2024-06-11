d2813643708088d06dcfcb47d888571df1c53edb
siyeonSon <87802191+siyeonSon@users.noreply.github.com>
2024-06-11 17:23:12 +0900

    :bug: fix(domain): resolve flyway sql syntax error (#478)

--- backend/streetdrop-domain/src/main/resources/db/migration/V20240606__add_announcement_table.sql
+++ backend/streetdrop-domain/src/main/resources/db/migration/V20240606__add_announcement_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS announcement (
     title VARCHAR(200) NOT NULL,
     content VARCHAR(3000) NOT NULL,
     created_at DATETIME(6) NOT NULL,
-    modified_at DATETIME(6) NOT NULL
+    modified_at DATETIME(6) NOT NULL,
+    PRIMARY KEY (announcement_id)
 )
