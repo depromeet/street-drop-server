3cb3b27f1f617f3375961ea25bcf852245213506
yunyoung1819 <yunyoung1819@hanmail.net>
2024-07-20 18:06:27 +0900

    :bug: fix(domain): modify AWS S3 Image URL at level policy

--- backend/streetdrop-domain/src/main/java/com/depromeet/level/data/LevelPolicy.java
+++ backend/streetdrop-domain/src/main/java/com/depromeet/level/data/LevelPolicy.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum LevelPolicy {
-    LEVEL_1("드랍스타터", "신규유저", "https://street-drop.s3.ap-northeast-2.amazonaws.com/USER_PROFILE/Level1.png", 1L, 0L, 5L),
-    LEVEL_2("프로 드랍퍼", "5개 이상 드랍한 유저", "https://street-drop.s3.ap-northeast-2.amazonaws.com/USER_PROFILE/Level2.png", 2L, 5L, 25L),
-    LEVEL_3("스폐셜 DJ", "25개 이상 드랍한 유저", "https://street-drop.s3.ap-northeast-2.amazonaws.com/USER_PROFILE/Level3.png", 3L, 25L, Long.MAX_VALUE);
+    LEVEL_1("드랍스타터", "신규유저", "https://street-drop2.s3.ap-northeast-2.amazonaws.com/USER_PROFILE/Level1.png", 1L, 0L, 5L),
+    LEVEL_2("프로 드랍퍼", "5개 이상 드랍한 유저", "https://street-drop2.s3.ap-northeast-2.amazonaws.com/USER_PROFILE/Level2.png", 2L, 5L, 25L),
+    LEVEL_3("스폐셜 DJ", "25개 이상 드랍한 유저", "https://street-drop2.s3.ap-northeast-2.amazonaws.com/USER_PROFILE/Level3.png", 3L, 25L, Long.MAX_VALUE);
 
     private final String levelName;
     private final String description;
