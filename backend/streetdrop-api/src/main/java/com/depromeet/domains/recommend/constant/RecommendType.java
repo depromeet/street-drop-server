5438995cbf11ec7f964cf149ef63486d7c491f5c
siyeonSon <siyeon0209@naver.com>
2024-09-23 11:50:06 +0900

    :sparkles: refactor(api): change RecommendType title according to the design

--- backend/streetdrop-api/src/main/java/com/depromeet/domains/recommend/constant/RecommendType.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/recommend/constant/RecommendType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RecommendType {
-    POPULAR_CHART_SONG("인기 있는 음악", 30, true),
-    RECENT_SONGS("많이 드랍된 음악", 15, true),
+    POPULAR_CHART_SONG("지금 인기 있는 음악", 30, true),
+    RECENT_SONGS("최근 드랍된 음악", 15, true),
     CHART_ARTIST("아티스트", 10, false);
 
     private final String title;
