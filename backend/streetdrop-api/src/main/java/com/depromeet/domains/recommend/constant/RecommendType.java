684c0a0bc528890dad8fd4b04581f883a7227076
siyeonSon <siyeon0209@naver.com>
2024-10-17 20:45:52 +0900

    :recycle: refactor(api): change response data "" -> null

--- backend/streetdrop-api/src/main/java/com/depromeet/domains/recommend/constant/RecommendType.java
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/recommend/constant/RecommendType.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RecommendType {
     POPULAR_CHART_SONG("지금 인기 있는 음악", "basic", "애플 뮤직의 '지금 인기 있는 곡' 리스트를 반영했어요.", 30, true),
-    RECENT_SONGS("최근 드랍된 음악", "basic", "", 15, true),
-    CHART_ARTIST("아티스트", "keyword", "", 10, false);
+    RECENT_SONGS("최근 드랍된 음악", "basic", null, 15, true),
+    CHART_ARTIST("아티스트", "keyword", null, 10, false);
 
     private final String title;
     private final String type;
